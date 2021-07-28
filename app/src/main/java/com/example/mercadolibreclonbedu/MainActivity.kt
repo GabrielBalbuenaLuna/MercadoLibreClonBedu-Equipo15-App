3120a38ae78fbc0b02f7affcf8678c2efad138e3
Elias Bautista <elias.bautistaflores@outlook.com>
2021-07-28 13:01:35 -0500

    Add LogIn & Register
    -Add LogIn view
    -Add Register view
    -Add colors

--- app/src/main/java/com/example/mercadolibreclonbedu/MainActivity.kt
+++ app/src/main/java/com/example/mercadolibreclonbedu/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.mercadolibreclonbedu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var buttonLogIn = findViewById<Button>(R.id.LogIn)
+        buttonLogIn.setOnClickListener {
+            val intent = Intent(this, LogIn::class.java)
+            startActivity(intent)
+        }
+
+        var buttonRegister = findViewById<Button>(R.id.Register)
+        buttonRegister.setOnClickListener{
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+        }
+
     }
+
 }
