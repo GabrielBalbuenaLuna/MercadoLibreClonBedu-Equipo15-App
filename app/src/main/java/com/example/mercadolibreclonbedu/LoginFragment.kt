84f5e5593a1e79b0ebeddaf7fab7aaa2798df91e
Ali Villegas <ali.villegas.aquino@gmail.com>
2021-08-10 01:37:17 -0500

    Bottom Navigation

--- app/src/main/java/com/example/mercadolibreclonbedu/LoginFragment.kt
+++ app/src/main/java/com/example/mercadolibreclonbedu/LoginFragment.kt
@@ -1,24 +1,26 @@
 package com.example.mercadolibreclonbedu
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.google.android.material.button.MaterialButton
-import kotlinx.android.synthetic.main.activity_my_account.*
 
-class MyAccountFragment : Fragment() {
-
+class LoginFragment : Fragment(){
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_my_account, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        val buttonLogin = view.findViewById<MaterialButton>(R.id.buttonLogin)
+
+        buttonLogin.setOnClickListener {
+            (activity as MainActivity).navigateTo(MainFragment(), false)
+        }
 
         return view
     }
-
 }
