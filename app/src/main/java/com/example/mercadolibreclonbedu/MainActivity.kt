f6810939fc12c0d6b5e914bf2813d6319dcf74d5
PaulinaQuintero <paulina.mucito@gmail.com>
2021-08-10 00:16:42 -0500

    Adding home view fragment to list products

--- app/src/main/java/com/example/mercadolibreclonbedu/MainActivity.kt
+++ app/src/main/java/com/example/mercadolibreclonbedu/MainActivity.kt
@@ -4,41 +4,45 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import androidx.fragment.app.Fragment
 import io.paperdb.Paper
 
 
 class MainActivity : AppCompatActivity() {
 
-    /*private lateinit var buttonProductView: Button
-    private lateinit var buttonFavoritesView: Button
-    private lateinit var buttonMyAccount: Button*/
+    private lateinit var buttonProductView: Button
 
+    private lateinit var buttonFavoritesView: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Paper.init(this)
         setContentView(R.layout.activity_main)
 
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                .add(R.id.fragment_container, LoginFragment())
-                .commit()
+        buttonProductView = findViewById(R.id.buttonProductView)
+        buttonFavoritesView = findViewById(R.id.buttonFavoritesView)
+
+
+        buttonProductView.setOnClickListener {
+            val intent1 = Intent(this, HomeActivity::class.java)
+            startActivity(intent1)
         }
 
-    }
-
-
-    fun navigateTo(fragment: Fragment, addToBackstack: Boolean) {
-        val transaction = supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.fragment_container, fragment)
-
-        if (addToBackstack) {
-            transaction.addToBackStack(null)
+        buttonFavoritesView.setOnClickListener {
+            val intent2 = Intent(this, FavoritesActivity::class.java)
+            startActivity(intent2)
         }
 
-        transaction.commit()
+        var buttonLogIn = findViewById<Button>(R.id.LogIn)
+        buttonLogIn.setOnClickListener {
+            val intent = Intent(this, LogIn::class.java)
+            startActivity(intent)
+        }
+
+        var buttonRegister = findViewById<Button>(R.id.Register)
+        buttonRegister.setOnClickListener{
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+        }
+
     }
 
 }
