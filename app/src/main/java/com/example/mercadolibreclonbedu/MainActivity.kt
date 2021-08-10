18bcc95234fe59a5f8a37b208ba10931c78f2bf8
Ali Villegas <ali.villegas.aquino@gmail.com>
2021-08-10 01:37:17 -0500

    Bottom Navigation

--- app/src/main/java/com/example/mercadolibreclonbedu/MainActivity.kt
+++ app/src/main/java/com/example/mercadolibreclonbedu/MainActivity.kt
@@ -4,51 +4,41 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import io.paperdb.Paper
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var buttonProductView: Button
+    /*private lateinit var buttonProductView: Button
     private lateinit var buttonFavoritesView: Button
-    private lateinit var buttonMyAccount: Button
+    private lateinit var buttonMyAccount: Button*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Paper.init(this)
         setContentView(R.layout.activity_main)
 
-        buttonProductView = findViewById(R.id.buttonProductView)
-        buttonFavoritesView = findViewById(R.id.buttonFavoritesView)
-        buttonMyAccount = findViewById(R.id.buttonMyAccount)
-
-
-        buttonProductView.setOnClickListener {
-            val intent1 = Intent(this, MainProductList::class.java)
-            startActivity(intent1)
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, LoginFragment())
+                .commit()
         }
 
-        buttonFavoritesView.setOnClickListener {
-            val intent2 = Intent(this, FavoritesActivity::class.java)
-            startActivity(intent2)
-        }
-        buttonMyAccount.setOnClickListener {
-            val intent3 = Intent(this, MyAccount::class.java)
-            startActivity(intent3)
+    }
+
+
+    fun navigateTo(fragment: Fragment, addToBackstack: Boolean) {
+        val transaction = supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+
+        if (addToBackstack) {
+            transaction.addToBackStack(null)
         }
 
-        var buttonLogIn = findViewById<Button>(R.id.LogIn)
-        buttonLogIn.setOnClickListener {
-            val intent = Intent(this, LogIn::class.java)
-            startActivity(intent)
-        }
-
-        var buttonRegister = findViewById<Button>(R.id.Register)
-        buttonRegister.setOnClickListener{
-            val intent = Intent(this, Register::class.java)
-            startActivity(intent)
-        }
-
+        transaction.commit()
     }
 
 }
