a07a86ec55c3e71a3b8575a8582f519884d91e5d
Elias Bautista <elias.bautistaflores@outlook.com>
2021-08-10 20:16:39 -0500

    Log changes

--- app/src/main/java/com/example/mercadolibreclonbedu/MainActivity.kt
+++ app/src/main/java/com/example/mercadolibreclonbedu/MainActivity.kt
@@ -19,10 +19,11 @@ class MainActivity : AppCompatActivity() {
         Paper.init(this)
         setContentView(R.layout.activity_main)
 
+
         if (savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
-                .add(R.id.fragment_container, LoginFragment())
+                .add(R.id.fragment_container, MainFragment())
                 .commit()
         }
 
