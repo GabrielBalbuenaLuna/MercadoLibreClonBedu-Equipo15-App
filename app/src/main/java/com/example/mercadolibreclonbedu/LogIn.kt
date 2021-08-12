4629fc749efcb20f05d0a1674bfc18d8b9f3b004
Elias Bautista <elias.bautistaflores@outlook.com>
2021-08-12 15:02:32 -0500

    Changes in Login and Register
    Add Progress bar
    Change Input type in password

--- app/src/main/java/com/example/mercadolibreclonbedu/LogIn.kt
+++ app/src/main/java/com/example/mercadolibreclonbedu/LogIn.kt
@@ -3,7 +3,9 @@ package com.example.mercadolibreclonbedu
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
 import kotlinx.android.synthetic.main.activity_log_in.*
@@ -13,6 +15,7 @@ class LogIn : AppCompatActivity() {
     private lateinit var buttonLog: Button
     private lateinit var user: TextInputEditText
     private lateinit var pass: TextInputEditText
+    private lateinit var progress: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +24,7 @@ class LogIn : AppCompatActivity() {
         buttonLog = findViewById(R.id.buttonLogin)
         user = findViewById(R.id.username)
         pass = findViewById(R.id.password)
+        progress = findViewById(R.id.progress)
 
 
 
@@ -28,6 +32,7 @@ class LogIn : AppCompatActivity() {
 
             if(user.text?.trim()!!.isNotEmpty() && password.text?.trim()!!.isNotEmpty()){
                 if (user.text?.trim().toString() == "root" && password.text?.trim().toString() == "1234"){
+                    progress.setVisibility(View.VISIBLE)
                     Toast.makeText(this, "Welcome :)", Toast.LENGTH_LONG).show()
                     val intent = Intent(this, MainActivity::class.java)
                     startActivity(intent)
